156ab5314e11982a52560dcc83fb54c9506a0824
Kate Pond <kate@thepondsedge.com>
2018-03-05 13:24:58 -0800

    sets up testing files

---
+++ specs/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'time'
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../lib/block_rooms'
+require_relative '../lib/hotel_admin'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
