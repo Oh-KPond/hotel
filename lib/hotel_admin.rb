e573a4f98d0240e81e572c5a5dd086ca673a9816
Kate Pond <kate@thepondsedge.com>
2018-03-05 17:19:28 -0800

    creates a test that a collection of rooms is created in HotelAdmin on init

--- lib/hotel_admin.rb
+++ lib/hotel_admin.rb
@@ -8,7 +8,13 @@ module HotelBooking
     attr_reader :all_rooms, :all_blocks, :reservations
 
     def initialize
-      @all_rooms = []
+
+      all_rooms = []
+      (1..20).each do |num|
+        all_rooms << HotelBooking::Room.new(num)
+      end
+
+      @all_rooms = all_rooms
       @all_blocks = []
       @reservations = []
 
--- specs/hotel_admin_spec.rb
+++ specs/hotel_admin_spec.rb
@@ -7,6 +7,7 @@ describe "Hotel Admin class" do
     end
 
     it "establishes the base data structures when instantiated" do
+
       @admin.must_be_instance_of HotelBooking::HotelAdmin
 
       @admin.must_respond_to :all_rooms
@@ -17,6 +18,14 @@ describe "Hotel Admin class" do
       @admin.all_blocks.must_be_kind_of Array
       @admin.reservations.must_be_kind_of Array
 
+    end
+
+    it "creates a collection of all_rooms" do
+
+      @admin.all_rooms.first.must_be_instance_of HotelBooking::Room
+
+      @admin.all_rooms.first.id.must_equal 1
+      @admin.all_rooms.last.id.must_equal 20
     end
   end
 end
