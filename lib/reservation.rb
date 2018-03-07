bef4fdcdd193d82df08617217790bb599b8f11de
Kate Pond <kate@thepondsedge.com>
2018-03-06 16:05:07 -0800

    makes rooms in reservation class sigular room

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -3,15 +3,15 @@ require 'date'
 module HotelBooking
   class Reservation
 
-    attr_reader :start_date, :num_nights, :rooms
+    attr_reader :start_date, :num_nights, :room
 
-    def initialize(first_night, num_nights, rooms)
+    def initialize(first_night, num_nights, room)
 
       start_date = Date.parse(first_night)
 
       @start_date = start_date
       @num_nights = num_nights
-      @rooms = rooms
+      @room = room
     end
   end
 end
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -6,8 +6,8 @@ describe "Reservation class" do
     before do
       first_night = '2018-10-3'
       num_nights = 3
-      rooms = [1,3]
-      @reservation = HotelBooking::Reservation.new(first_night, num_nights, rooms)
+      room = 1
+      @reservation = HotelBooking::Reservation.new(first_night, num_nights, room)
     end
 
     it "establishes the base data structures when instantiated" do
@@ -16,11 +16,11 @@ describe "Reservation class" do
 
       @reservation.must_respond_to :start_date
       @reservation.must_respond_to :num_nights
-      @reservation.must_respond_to :rooms
+      @reservation.must_respond_to :room
 
       @reservation.start_date.must_be_kind_of Date
       @reservation.num_nights.must_be_kind_of Integer
-      @reservation.rooms.must_be_kind_of Array
+      @reservation.room.must_be_kind_of Integer
 
     end
   end
