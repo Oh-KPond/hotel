dca25a190d5be971f662ac06f88faa7bbce16007
Kate Pond <kate@thepondsedge.com>
2018-03-07 16:58:32 -0800

    makes cost per night a constant within room

--- lib/room.rb
+++ lib/room.rb
@@ -3,13 +3,15 @@ module HotelBooking
 
     attr_reader :id, :cost
 
+    COST_PER_NIGHT = 200.00
+    
     def initialize(id)
       unless id > 0
         raise ArgumentError.new("ID must be a positive number.")
       end
-      
+
       @id = id
-      @cost = 200.00
+      @cost = COST_PER_NIGHT
     end
 
   end
